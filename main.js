f48dc080587e5406ca27b326e7bc71902faea04c
Fareed118 <foforedo@hotmail.com>
2019-12-03 15:58:00 +0300

    Fareed Kanuri HW

---
+++ main.js
@@ -0,0 +1,11 @@
+    // With the element initially hidden, we can show it slowly:
+    $( "#clicker" ).click(function() {
+        
+      });
+
+       //With the element initially hidden, we can show it slowly:
+$( "#clickme" ).click(function() {
+    $( "#book" ).fadeIn( "slow", function() {
+      // Animation complete
+    });
+  });
